74e01643e7c4c13ce7c8be60e8050400bbb74e67
Bilguun <bilguun@LEAP-03s-iMac.local>
2022-05-02 19:12:28 +0800

    one day

---
+++ 01_Day_Introdaction/variable.js
@@ -0,0 +1,30 @@
+const text = "hello World";
+const boolean = true;
+let firstsName
+let emptyValue = null;
+const number = 5;
+const one = "";
+
+console.log(text, boolean, one, firstsName, number, emptyValue);
+
+const four = "one";
+const three = "two";
+const two = "three";
+const onee = "one";
+
+console.log(four, three, two, onee);
+
+let firstName = "-Tumurbaatar";
+let lastName = "-Bilguun";
+let family = "-wife one son"; 
+let live = "-Mongolia";
+let age = "-I'm 27 years old"
+
+console.log(firstName);
+console.log(lastName);
+console.log(family);
+console.log(live);
+console.log(age);
+
+let names = `bilguun`, from = 'mongolia', agee = 25;
+console.log(names, from, agee);
